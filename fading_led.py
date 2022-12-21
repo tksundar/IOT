d4ad249742c2383f0f9170ccc26e1196c594fd5d
tksundar <53440744+tksundar@users.noreply.github.com>
2022-12-21 16:27:18 +0530

    First Commit

---
+++ fading_led.py
@@ -0,0 +1,31 @@
+import math
+import utime
+
+import machine
+
+# create a PWM object
+led_builtin = machine.PWM(machine.Pin(2), freq=1000)
+led_pin5 = machine.PWM(machine.Pin(5), freq=1000)
+
+
+# define a function to light the led with varying powersupply
+def pulse(led, t):
+    for i in range(20):
+        on_ratio = math.sin(i / 10 * math.pi) * 500 + 500
+        if i == 19:
+            print(on_ratio)
+        else:
+            print(on_ratio, end="|")
+        led.duty(int(on_ratio))
+        utime.sleep_ms(t)
+
+    led.duty(int(1500))
+
+
+for i in range(10):
+    pulse(led_builtin, 50)
+
+for i in range(10):
+    pulse(led_pin5, 50)
+# Turn off the external led. set duty cycle to 0
+led_pin5.duty(0)
---
+++ mqtt_client.py
@@ -0,0 +1,75 @@
+import network
+import sys
+import time
+
+from machine import Pin
+from umqtt.robust import MQTTClient
+
+
+def connect_to_network(wlan, ssid, password):
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('Connecting to network...')
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            print('.', end='')
+            time.sleep(3)
+        print()
+        print('Connected:', wlan.isconnected())
+
+    # get the interface's IP/netmask/gw/DNS addresses
+    print("ip config router: " + str(wlan.ifconfig()))
+
+
+def do_blink(led, off_value):
+    for x in range(10):
+        led.off()
+        time.sleep(0.5)
+        led.on()
+        time.sleep(0.5)
+    led.value(off_value)
+
+
+def blink():
+    led_builtin = Pin(2, Pin.OUT)
+    led5 = Pin(5, Pin.OUT)
+    do_blink(led_builtin, 1)
+    do_blink(led5, 0)
+    client.disconnect()
+    sys.exit()
+
+
+def on_message(topic, msg):
+    command = msg.decode()
+    if command == "BLINK":
+        print("received message " + command)
+        blink()
+
+
+def subscribe(topic):
+    while not done:
+        client.subscribe(topic)
+        time.sleep(3)
+
+
+def connect_to_broker():
+    if sta_if.isconnected():
+        client.connect()
+        print("subscribing to topic " + topic.decode())
+        subscribe(topic)
+    else:
+        print("No network")
+
+
+# ---Main code starts from here---
+SSID = "SUNDAR";
+password = "Tksrajan4$"
+sta_if = network.WLAN(network.STA_IF)
+if sta_if.isconnected():
+    sta_if.disconnect()
+client_id = "ESP8266"
+client = MQTTClient(client_id, "test.mosquitto.org", port=1883)
+topic = 'led/command'.encode()
+client.set_callback(on_message)
+connect_to_network(sta_if, SSID, password)
+connect_to_broker()
